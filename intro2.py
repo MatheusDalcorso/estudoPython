5b962f85c65aa0910252c3ca9758a47cd8c70750
Matheus de Carvalho Dalcorso <matheus_dalcorso@outlook.com>
2025-09-12 17:24:22 -0300

    Adicionado conteúdo

--- intro2.py
+++ intro2.py
@@ -39,12 +39,68 @@
 # Além dessas comparações que coloquei acima, tamém temos operações booleanas que podemos usar
 # Elas são: and (&&), or (||) e not (!)
 
-user = 'admin'
-logged_in = False
+# user = 'admin'
+# logged_in = False
 
-if user == 'admin' and (not logged_in):
-    print('Página do Admin')
+# if user == 'admin' or logged_in:
+#     print('Página do Admin')
+# else:
+#     print('Acesso negado')
+
+# if user == 'admin' and not (not logged_in):
+#     print('Página do Admin')
+# else:
+#     print('Acesso negado')
+
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# c = a
+# print("a = [1, 2, 3]\nb = [1, 2, 3]\nc = a")
+# print('')
+# print('Teste com "b"')
+# print(f"a == b : {a == b}")
+# print(f"a is b : {a is b}")
+# print(f"id 'a': {id(a)} \nid 'b': {id(b)}")
+# print('')
+# print('Teste com "c"')
+# print(f"a == c : {a == c}")
+# print(f"a is c : {a is c}")
+# print(f"id 'a': {id(a)} \nid 'c': {id(c)}")
+
+# a comparação com is funciona basicamente como id(variavel1) == id(variavel2)
+
+
+# False Values:
+    # False
+    # None     (Python does not have a literal 'null' keyword. None é basicamente o null de python)
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.    
+# (A mapping is a collection that allows you to look up a key and retrieve its value. https://realpython.com/python-mappings/)
+
+# Todo resto é True
+
+condition = [""]
+
+if condition:
+    print('Evaluated to True')
 else:
-    print('Acesso negado')
+    print('Evaluated to False')
+
+#exemplo de uso, preciso checar alguma função retornou uma string com valores
+string = ''
+
+if string:
+    print('String com algum valor')
+else:
+    print('String vazia')
+ 
+
+
+
+
+
+
 
 
