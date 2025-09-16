a5d044a25657b0255e5ad3708c3b9df7aba109df
Matheus de Carvalho Dalcorso <matheus_dalcorso@outlook.com>
2025-09-16 18:20:32 -0300

    adicionado conteúdo e criado pasta de exercícios

---
+++ exercicios/hello-world.py
@@ -0,0 +1,36 @@
+# import this
+
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+#https://inventwithpython.com/blog/zen-of-python-explained.html
+
+
+# print("          hello, world!        ".title().strip())
+
+# for i in "          hello, world!        ".title().strip():
+#     print(i, end='')
+    #o 'end' modifica o funcionamento do print, que por padrão pula de linha depois de imprimir.
+    # No caso do "end=''" ele coloca uma string vazia depois de imprimir ao invés de pular uma linha.
+    # Colocando "end='!'" a frase ficaria assim -> H!e!l!l!o!,! !W!o!r!l!d!!!
+
+print('Hello, world!')
