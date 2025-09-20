cae5ef9f99bcf6fdd135d311368cb859c7cca91b
Matheus de Carvalho Dalcorso <matheus_dalcorso@outlook.com>
2025-09-20 00:27:07 -0300

    Adicionado conteúdo sobre list, dictionary e set comprehention

--- intro.py
+++ intro.py
@@ -139,12 +139,75 @@
 # empty_set = set()
 
 
+
+
+
+
+# dictionaries
+# https://www.w3schools.com/python/python_dictionaries.asp
+# dictionaries nos permitem trabalhar com pares de key:value
+
+# message = "Hello, world!"
+# courses = ['history', 'math', 'phisics', 'compSci']
+
+# student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+# student = {'name': 'John', 'age': 25, 'courses': courses}
+
+# print(student)
+
+# print(student['name'])
+# print(student['age'])
+# print(student['courses'])
+# print(student['courses'][0])
+# print(student['courses'][1])
+
+# print(student.get('name'))
+# print(student.get('phone'))
+# print(student.get('name', 'No key found'))
+# print(student.get('phone', courses))
+
+# student['phone'] = '3625-3625'
+
+# print(student.get('phone', 'No key found'))
+
+
+# student['name'] = 'Matt'
+# student.update({'name': 'Lucas', 'age': '30', 'id': '1'})
+
+# print(student)
+
+
+# del student['id']
+
+# print(student)
+
+# phone = student.pop('phone')
+# print(student)
+# print(phone)
+
+
+
+# print(len(student))
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+# for key in student.items():
+#     print(type(key))
+
+# for key, value in student.items():
+    # print(key, value)
+
+
+
+
+
 # List comprehension
 # É uma maneira mais compacta de criar uma lista em python. Ao invés de usar um loop e colocar a lista dentro fazendo append, a gente coloca esse loop dentro da lista pra criar ela.
 # "List comprehensions in Python provide a concise way to create lists by embedding a loop and optional conditional logic in a single line." (https://realpython.com/list-comprehension-python/)
 
 # ex de como seria feito sem a list comprehension
-nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] # pra usar nos exemplos
+# nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] # pra usar nos exemplos
 
 # my_list = []
 
@@ -175,7 +238,7 @@ nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] # pra usar nos exemplos
 # my_list = map(lambda n: n*n, nums)
 # print(list(my_list))
 
-# n pra cada n in nuns se n for par
+# n pra cada n de nuns se n for par
 # sem list comprehension
 # my_list = []
 
@@ -192,60 +255,70 @@ nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] # pra usar nos exemplos
 # my_list = filter(lambda n: n % 2 ==0, nums)
 # print(list(my_list))
 
+# par de (letra, numero) pra cada letra em 'abcd' e cada numero em '0123'
+# sem list comprehension
+# my_list = []
+# for letter in 'abcd':
+#   for num in range(4):
+#     my_list.append((letter,num))
+# print(my_list)
 
-# dictionaries
-# https://www.w3schools.com/python/python_dictionaries.asp
-# dictionaries nos permitem trabalhar com pares de key:value
+# com list comprehension
+# my_list = [(letter, num) for letter in 'abcd' for num in range(4)]
+# print(my_list)
 
-message = "Hello, world!"
-courses = ['history', 'math', 'phisics', 'compSci']
+# exercicio de list comprehension do HackerRank
+# x = 1
+# y = 1
+# z = 1
+# n = 2
 
-student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
-# student = {'name': 'John', 'age': 25, 'courses': courses}
-
-# print(student)
-
-# print(student['name'])
-# print(student['age'])
-# print(student['courses'])
-# print(student['courses'][0])
-# print(student['courses'][1])
-
-# print(student.get('name'))
-# print(student.get('phone'))
-# print(student.get('name', 'No key found'))
-# print(student.get('phone', courses))
-
-student['phone'] = '3625-3625'
-
-# print(student.get('phone', 'No key found'))
-
-
-# student['name'] = 'Matt'
-student.update({'name': 'Lucas', 'age': '30', 'id': '1'})
-
-# print(student)
-
-
-del student['id']
-
-# print(student)
-
-phone = student.pop('phone')
-# print(student)
-# print(phone)
+# my_list = [ [num1, num2, num3] for num1 in range(x+1) for num2 in range(y+1) for num3 in range(z+1) if num1 + num2 + num3 != n ]
+# print(my_list)
 
 
 
-# print(len(student))
-# print(student.keys())
-# print(student.values())
-# print(student.items())
+# Dictionary comprehensions
+# funciona da mesma maneira que as listas
 
-# for key in student.items():
-#     print(type(key))
+# names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
+# heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
+# print(list(zip(names, heros)))
 
-# for key, value in student.items():
-    # print(key, value)
+# dicionario {'nome': 'heroi'} pra cada nome, heroi no zip(names, heros)
+# sem dictionary comprehensions
+# my_dict = {}
+# for name, hero in zip(names, heros):
+#     my_dict[name] = hero
+# print(my_dict)
+
+# com dictionary comprehensions
+# my_dict = {name: hero for name, hero in zip(names, heros)}
+# print(my_dict)
+
+# o mesmo exercicio mas só coloca no dicionario se o nome não for Peter
+# sem dictionary comprehensions
+# my_dict = {}
+# for name, hero in zip(names, heros):
+#     if name != 'Peter':
+#         my_dict[name] = hero
+# print(my_dict)
+
+# com dictionary comprehensions
+# my_dict = {name: hero for name, hero in zip(names, heros) if name != 'Peter'}
+# print(my_dict)
 
 
+
+# Set Comprehensions
+# funciona da mesma maneira, só um exemplo mesmo:
+# nums = [1,1,2,1,3,4,3,4,5,5,6,7,8,7,9,9]
+# sem set comprehension
+# my_set = set()
+# for n in nums:
+#     my_set.add(n)
+# print(my_set)
+
+# com set comprehension
+# my_set = {n for n in nums}
+# print(my_set)
